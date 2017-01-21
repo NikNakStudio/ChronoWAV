e420dbae02e0c7d19727b3ad32c0e3fc995b5e9d
Juan Hernández García <juanhgw@gmail.com>
2017-01-21 17:42:24 +0100

    Added Assets

---
+++ Scripts/Dimension.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dimension : MonoBehaviour {
+
+    public GameObject tailWaveObject;
+    public GameObject headerWaveObject;
+    public int numberOfPoints= 100;
+    public float animSpeed =1.0f;
+    public float scaleInputRange = 2*Mathf.PI; // scale number from [0 to 99] to [0 to 2Pi]
+    public float scaleResult = 10.0f;
+    public float frecuency = 1;
+    public bool animate = true;
+    public float motionSpeed = 0;
+    public Color backgroundColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Scripts/MainCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+	public LevelController levelController;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var headerPosition = levelController.GetWaveHeader().transform.position;
+		transform.position =  new Vector3(headerPosition.x + 5, 0, -10);
+	}
+}

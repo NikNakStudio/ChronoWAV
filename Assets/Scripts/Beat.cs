4325991fc901d07e4339eb9a6af3538f010799fa
juanhg <juanhgw@gmail.com>
2017-02-05 19:22:17 +0100

    Improved beat behaviour.

--- Assets/Scripts/Beat.cs
+++ Assets/Scripts/Beat.cs
@@ -4,32 +4,59 @@ using UnityEngine;
 
 public class Beat : MonoBehaviour {
 
-    public float beatPeriod = 2;
-    public float scaleFactor = 1.5F;
-    private float counter = 0.0f;
-    private float x;
-    private float y;
-    private float z;
+    public float beatPeriod = 2; //Valid values (0-inf)
+    public float maxScaleFactor = 1.5F; // Valid values [0-1]
+    public float diastoleFactor = 0.125f;// Valid values [0-1]
+
+    private float counter = 0f;
+    private float x, y, z, m1, m2, diastole, fix;
 
 	// Use this for initialization
 	void Start () {
+        InitValues();
+	}
+	
+    void InitValues() {
         x = gameObject.transform.localScale.x;
         y = gameObject.transform.localScale.y;
         z = gameObject.transform.localScale.z;
-	}
-	
+
+        diastole = diastoleFactor * beatPeriod;
+        m1 = (maxScaleFactor - 1f) / diastole;
+        m2 = (1f - maxScaleFactor) / (beatPeriod - diastole);
+        fix = applyF1(diastole) - (m2 *diastole);
+    }
+
 	// Update is called once per frame
 	void Update () {
         counter += Time.deltaTime;
-        if (counter <= beatPeriod / 2.0f)
-            ResetScale();
-        else if (counter >= beatPeriod / 2.0f && counter < beatPeriod)
-            Scale();
-        else
-            counter = 0;
+        var factor = getScaleFactor();
+        Scale(factor);
+
+        if (counter > beatPeriod)
+            counter = 0f;
     }
-      
-    void Scale(){
+
+    float getScaleFactor(){
+        var factor = 1f;
+
+        if (counter <= diastole)
+            factor = applyF1(counter);
+        else if (counter <= beatPeriod)
+            factor = applyF2(counter);
+
+        return factor;
+    }
+
+    float applyF1(float x){
+        return m1 * x + 1f;
+    }
+
+    float applyF2(float x){
+        return m2 * x + fix;
+    }
+        
+    void Scale(float scaleFactor){
         gameObject.transform.localScale = new Vector3(x*scaleFactor, y*scaleFactor, z);
     }
 
